42613c05d7107b7d7545aa737010954be044f910
wisemendevp <dev@vqs.com.au>
2016-05-13 15:14:18 +0530

    Update compress.js

--- www/js/compress.js
+++ www/js/compress.js
@@ -1,11 +1,11 @@
 var compress = {
-compress: function(success, failure,resultType){
+get: function(success, failure,resultType){
     
     var data = resultType;
    cordova.exec(success, failure, "compress", "compress", [data]);
 }
 };
 
-module.exports=compress;
+module.exports = compress;
 
 
